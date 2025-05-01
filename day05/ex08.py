9e59440686b82ab6c5df5a3f1088197ffe69aa74
Azuo <xiuhuizuo@gmail.com>
2025-05-01 23:56:36 +0800

    The 5th day's learning variable

--- day05/ex08.py
+++ day05/ex08.py
@@ -3,9 +3,9 @@ b = float(input('请输入三角形的边长：'))
 c = float(input('请输入三角形的边长：'))
 if a + b > c and a + c > b and b + c > a:
     perimeter = a + b + c
-    print(f'{perimeter = }')
+    print(f'周长：{perimeter}')
     s = perimeter / 2
     Area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
-    print(f'{Area = }')
+    print(f'面积：{Area}')
 else:
     print('不能构成三角形')
