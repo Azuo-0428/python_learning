5968fb29d439aa629ec600fe14ea54c59a09724e
Azuo <xiuhuizuo@gmail.com>
2025-05-06 00:23:43 +0800

    The 9th day's learning

---
+++ day08/ex05.py
@@ -0,0 +1,13 @@
+"""
+
+列表的关系运算（逐位比较）
+
+"""
+
+nums1 = [1, 2, 3, 4]
+nums2 = list(range(1, 5))
+nums3 = [3, 2, 1]
+print(nums1 == nums2)
+print(nums1 != nums2)
+print(nums1 <= nums3)
+print(nums2 >= nums3)
---
+++ day08/ex06.py
@@ -0,0 +1,14 @@
+"""
+
+元素的遍历
+
+"""
+
+languages = ['Python','Java','C++','Kotlin']
+for index in range(len(languages)):
+    print(languages[index])
+
+print("-------------------------")
+
+for language in languages:
+    print(language)
