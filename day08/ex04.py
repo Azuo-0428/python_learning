dba52bfe9f58a60c4300ab2152ab021e193b5b50
Azuo <xiuhuizuo@gmail.com>
2025-05-06 00:00:19 +0800

    The 9th day's learning

--- day08/ex04.py
+++ day08/ex04.py
@@ -15,3 +15,10 @@ print(items8[-4])
 print(items8[-1])
 items8[-4] = 'strawberry'
 print(items8)
+print(items8[1:3:1])
+print(items8[0:3:1])
+print(items8[0:5:2])
+print(items8[-4:-2:1])
+print(items8[-2:-6:-1])
+items8[1: 3] = ['x', 'o']
+print(items8)
