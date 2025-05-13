471b00e96f56ae9323517458385238a7d90a78e2
Azuo <xiuhuizuo@gmail.com>
2025-05-13 23:53:45 +0800

    formatting

--- day11/ex02.py
+++ day11/ex02.py
@@ -1,4 +1,18 @@
+"""
+
+转义字符
+
+"""
 s1 = '\'hello, world\''
 print(s1)
-s2= '\\hello,world\\'
+s2 = '\\hello,world\\'
 print(s2)
+
+"""
+原始字符串
+
+"""
+s3 = '\it \is \time \to \read \now'
+s4 = r'\it \is \time \to \read \now'
+print(s3)
+print(s4)
