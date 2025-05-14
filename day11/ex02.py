683ab5d5a9ea73e8385dd793e809680839df4fe1
Azuo <xiuhuizuo@gmail.com>
2025-05-14 23:53:12 +0800

    learning

--- day11/ex02.py
+++ day11/ex02.py
@@ -16,3 +16,14 @@ s3 = '\it \is \time \to \read \now'
 s4 = r'\it \is \time \to \read \now'
 print(s3)
 print(s4)
+
+"""
+
+字符的特殊表示
+
+"""
+
+s5 = '\141\142\143\x61\x62\x63'
+s6 = '\u9a86\u660a'
+print(s5)
+print(s6)
