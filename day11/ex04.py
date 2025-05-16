d481884c2cb9f5d7420917fa217dc142245353b6
Azuo <xiuhuizuo@gmail.com>
2025-05-16 23:09:25 +0800

    Come on

---
+++ day11/ex04.py
@@ -0,0 +1,22 @@
+"""
+
+比较运算
+
+"""
+
+s1 = 'a whole new world'
+s2 = 'hello world'
+print(s1 == s2)
+print(s1 >= s2)
+print(s1 < s2)
+print(s1 > s2)
+print(s1 != s2)
+s3 = '李琰'
+s4 = '陆溓宁'
+print(ord('李'))
+print(ord('琰'))
+print(ord('陆'))
+print(ord('溓'))
+print(ord('宁'))
+print(s3 >= s4)
+print(s3 != s4)
