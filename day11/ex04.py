8feda35b5f741445af542001d8be70df15ff9aec
Azuo <xiuhuizuo@gmail.com>
2025-05-17 22:08:22 +0800

    Come on

--- day11/ex04.py
+++ day11/ex04.py
@@ -20,3 +20,21 @@ print(ord('溓'))
 print(ord('宁'))
 print(s3 >= s4)
 print(s3 != s4)
+
+"""
+
+成员运算
+
+"""
+
+s1 = 'hello, world'
+s2 = 'goodbye world'
+print('wo' in s1)
+print('wo' not in s2)
+print(s2 in s1)
+
+s1 = 'hello, world'
+s2 = 'goodbye, world'
+print('wo' in s1)      # True
+print('wo' not in s2)  # False
+print(s2 in s1)        # False
