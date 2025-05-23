16d5f18f74fecd7f6b5a386715bf00c6b8e939d0
Azuo <xiuhuizuo@gmail.com>
2025-05-23 22:19:52 +0800

    learning...

--- day11/ex05.py
+++ day11/ex05.py
@@ -9,4 +9,12 @@ s1 = 'hello, world'
 print(s1.capitalize())
 # 字符串每个单词首字母大写
 print(s1.title())
+# 字符串变大写
+print(s1.upper())
+s2 = 'GOODBYE'
+# 字符串变小写
+print(s2.lower())
+# 检查s1和s2的值
+print(s1)
+print(s2)
 
