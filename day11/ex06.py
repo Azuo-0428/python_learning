71f0cd9443c615281636afd623fab19b8970ee57
Azuo <xiuhuizuo@gmail.com>
2025-05-28 00:07:18 +0800

    learning...

--- day11/ex06.py
+++ day11/ex06.py
@@ -27,7 +27,27 @@ print(s3.replace('o','@',1))
 
 """
 
-s4 = 'I love you'
+s4 = 'I love you so much'
 words = s4.split() #将一个字符串拆成多个字符串（放在同一列表中）
 print(words)
 print('~'.join(words))
+print('#'.join(words))
+words1= s4.split('#')
+print(words1)
+s5 = 'I#love#you#so#much'
+words2 = s5.split('#',2)
+print(words2)
+
+"""
+
+编码和解码
+
+"""
+
+a= 'Azuo'
+b = a.encode('utf-8')
+c = a.encode('gbk')
+print(b)
+print(c)
+print(b.decode('utf-8'))
+print(c.decode('gbk'))
