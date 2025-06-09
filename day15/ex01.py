b247551389dbd6780c0879ef6c55b32f6acc7df5
Azuo <xiuhuizuo@gmail.com>
2025-06-09 23:42:59 +0800

    14th day learning...

--- day15/ex01.py
+++ day15/ex01.py
@@ -10,10 +10,13 @@ print(string.digits)
 
 ALL_CHARS = string.digits + string.ascii_letters
 
-def generate_code(*, code_len=4):
+def generate_code(*, code_len=4): # 入参
     """
     生成指定长度的验证码
     :param code_len: 验证码的长度（默认4个字符）
     :return: 由大小写英文字母何数字构成的随机验证码字符串
     """
     return ''.join(random.choices(ALL_CHARS, k = code_len))
+
+for _ in range(5):
+    print(generate_code())
